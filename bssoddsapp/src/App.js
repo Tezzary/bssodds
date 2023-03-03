1271a9048735f23ca6e749388381985019e1cf9b
Tezzy <Tezzy>
2023-03-03 23:28:44 +1100

    new

--- bssoddsapp/src/App.js
+++ bssoddsapp/src/App.js
@@ -12,62 +12,32 @@ function App() {
 }
 function BeeSelect(props){
   
-  console.log(props)
+  const [bees, setbees] = useState([])
   const mystyle = {
       left: `${props.beeSelectPosition.x}px`,
       top: `${props.beeSelectPosition.y}px`
   };
+  useEffect(() => {
+    fetch("/static/miscellaneous/bees.json").then(res => res.json()).then(data => {
+      console.log(data.bees[0])
+      setbees([])
+      for (let i = 0; i < data.bees.length; i++) {
+        let bee = data.bees[i]
+        if(bee.rarity == "Common" || bee.rarity == "Event"){
+          continue
+        }
+        setbees(bees => [...bees, bee])
+      }
+    })
+  }, []) 
   return (
     <div className="BeeSelectWrapper" style={mystyle}>
       <h1>Choose a Bee</h1>
       <div className="BeeSelect">
-        
-      <button onClick={() => props.callback("BabyBee.png")}><img src="http://localhost:8000/static/beeimages/BabyBee.png"></img></button>
-      <button onClick={() => props.callback("BasicBee.png")}><img src="http://localhost:8000/static/beeimages/BasicBee.png"></img></button>
-      <button onClick={() => props.callback("BearBee.png")}><img src="http://localhost:8000/static/beeimages/BearBee.png"></img></button>
-      <button onClick={() => props.callback("BomberBee.png")}><img src="http://localhost:8000/static/beeimages/BomberBee.png"></img></button>
-      <button onClick={() => props.callback("BraveBee.png")}><img src="http://localhost:8000/static/beeimages/BraveBee.png"></img></button>
-      <button onClick={() => props.callback("BubbleBee.png")}><img src="http://localhost:8000/static/beeimages/BubbleBee.png"></img></button>
-      <button onClick={() => props.callback("BuckoBee.png")}><img src="http://localhost:8000/static/beeimages/BuckoBee.png"></img></button>
-      <button onClick={() => props.callback("BumbleBee.png")}><img src="http://localhost:8000/static/beeimages/BumbleBee.png"></img></button>
-      <button onClick={() => props.callback("BuoyantBee.png")}><img src="http://localhost:8000/static/beeimages/BuoyantBee.png"></img></button>
-      <button onClick={() => props.callback("CarpenterBee.png")}><img src="http://localhost:8000/static/beeimages/CarpenterBee.png"></img></button>
-      <button onClick={() => props.callback("CobaltBee.png")}><img src="http://localhost:8000/static/beeimages/CobaltBee.png"></img></button>
-      <button onClick={() => props.callback("CommanderBee.png")}><img src="http://localhost:8000/static/beeimages/CommanderBee.png"></img></button>
-      <button onClick={() => props.callback("CoolBee.png")}><img src="http://localhost:8000/static/beeimages/CoolBee.png"></img></button>
-      <button onClick={() => props.callback("CrimsonBee.png")}><img src="http://localhost:8000/static/beeimages/CrimsonBee.png"></img></button>
-      <button onClick={() => props.callback("DemoBee.png")}><img src="http://localhost:8000/static/beeimages/DemoBee.png"></img></button>
-      <button onClick={() => props.callback("DemonBee.png")}><img src="http://localhost:8000/static/beeimages/DemonBee.png"></img></button>
-      <button onClick={() => props.callback("DiamondBee.png")}><img src="http://localhost:8000/static/beeimages/DiamondBee.png"></img></button>
-      <button onClick={() => props.callback("DigitalBee.png")}><img src="http://localhost:8000/static/beeimages/DigitalBee.png"></img></button>
-      <button onClick={() => props.callback("ExhaustedBee.png")}><img src="http://localhost:8000/static/beeimages/ExhaustedBee.png"></img></button>
-      <button onClick={() => props.callback("FestiveBee.png")}><img src="http://localhost:8000/static/beeimages/FestiveBee.png"></img></button>
-      <button onClick={() => props.callback("FireBee.png")}><img src="http://localhost:8000/static/beeimages/FireBee.png"></img></button>
-      <button onClick={() => props.callback("FrostyBee.png")}><img src="http://localhost:8000/static/beeimages/FrostyBee.png"></img></button>
-      <button onClick={() => props.callback("FuzzyBee.png")}><img src="http://localhost:8000/static/beeimages/FuzzyBee.png"></img></button>
-      <button onClick={() => props.callback("GummyBee.png")}><img src="http://localhost:8000/static/beeimages/GummyBee.png"></img></button>
-      <button onClick={() => props.callback("HastyBee.png")}><img src="http://localhost:8000/static/beeimages/HastyBee.png"></img></button>
-      <button onClick={() => props.callback("HoneyBee.png")}><img src="http://localhost:8000/static/beeimages/HoneyBee.png"></img></button>
-      <button onClick={() => props.callback("LionBee.png")}><img src="http://localhost:8000/static/beeimages/LionBee.png"></img></button>
-      <button onClick={() => props.callback("LookerBee.png")}><img src="http://localhost:8000/static/beeimages/LookerBee.png"></img></button>
-      <button onClick={() => props.callback("MusicBee.png")}><img src="http://localhost:8000/static/beeimages/MusicBee.png"></img></button>
-      <button onClick={() => props.callback("NinjaBee.png")}><img src="http://localhost:8000/static/beeimages/NinjaBee.png"></img></button>
-      <button onClick={() => props.callback("PhotonBee.png")}><img src="http://localhost:8000/static/beeimages/PhotonBee.png"></img></button>
-      <button onClick={() => props.callback("PreciseBee.png")}><img src="http://localhost:8000/static/beeimages/PreciseBee.png"></img></button>
-      <button onClick={() => props.callback("PuppyBee.png")}><img src="http://localhost:8000/static/beeimages/PuppyBee.png"></img></button>
-      <button onClick={() => props.callback("RadBee.png")}><img src="http://localhost:8000/static/beeimages/RadBee.png"></img></button>
-      <button onClick={() => props.callback("RageBee.png")}><img src="http://localhost:8000/static/beeimages/RageBee.png"></img></button>
-      <button onClick={() => props.callback("RascalBee.png")}><img src="http://localhost:8000/static/beeimages/RascalBee.png"></img></button>
-      <button onClick={() => props.callback("RileyBee.png")}><img src="http://localhost:8000/static/beeimages/RileyBee.png"></img></button>
-      <button onClick={() => props.callback("ShockedBee.png")}><img src="http://localhost:8000/static/beeimages/ShockedBee.png"></img></button>
-      <button onClick={() => props.callback("ShyBee.png")}><img src="http://localhost:8000/static/beeimages/ShyBee.png"></img></button>
-      <button onClick={() => props.callback("SpicyBee.png")}><img src="http://localhost:8000/static/beeimages/SpicyBee.png"></img></button>
-      <button onClick={() => props.callback("StubbornBee.png")}><img src="http://localhost:8000/static/beeimages/StubbornBee.png"></img></button>
-      <button onClick={() => props.callback("TabbyBee.png")}><img src="http://localhost:8000/static/beeimages/TabbyBee.png"></img></button>
-      <button onClick={() => props.callback("TadpoleBee.png")}><img src="http://localhost:8000/static/beeimages/TadpoleBee.png"></img></button>
-      <button onClick={() => props.callback("VectorBee.png")}><img src="http://localhost:8000/static/beeimages/VectorBee.png"></img></button>
-      <button onClick={() => props.callback("ViciousBee.png")}><img src="http://localhost:8000/static/beeimages/ViciousBee.png"></img></button>
-      <button onClick={() => props.callback("WindyBee.png")}><img src="http://localhost:8000/static/beeimages/WindyBee.png"></img></button>
+        {bees.map(bee => (
+          <button onClick={() => props.callback(bee)}><img src={"/static/beeimages/" + bee.name + ".png"}></img></button>
+        ))}
+
       
       
       </div>
@@ -75,11 +45,13 @@ function BeeSelect(props){
   );
 }
 function RoyalJelly() {
-  let [src, setSrc] = useState("http://localhost:8000/static/beeimages/BasicBee.png");
+  let [src, setSrc] = useState("/static/beeimages/BasicBee.png");
   let [beeSelectIsOn, setbeeSelectIsOn] = useState(false);
   let [beeSelectPosition, setBeeSelectPosition] = useState({x: 600, y: 100});
-  let [activeBee, setActiveBees] = useState("BasicBee.png");
+  let [activeBee, setActiveBees] = useState({name: "VectorBee", rarity: "Mythic"});
   const [mousePos, setMousePos] = useState({x: 600, y: 100});
+  const [royalJellyCount, setRoyalJellyCount] = useState(0);
+  const [chance, setChance] = useState(0);
   useEffect(() => {
     const handleMouseMove = (event) => {
       setMousePos({x: event.clientX, y: event.clientY});
@@ -94,18 +66,42 @@ function RoyalJelly() {
     setActiveBees(bee)
     beeSelectOn()
   }
-
+  let calculate = () => {
+    let chanceOfNotGetting = 1
+    if(activeBee.rarity == "Mythic"){
+      chanceOfNotGetting = 1 - 1 / (25000 * 6)
+    }
+    else if(activeBee.rarity == "Legendary"){
+      chanceOfNotGetting = 1 - 3 / (100 * 8)
+    }
+    else if(activeBee.rarity == "Epic"){
+      chanceOfNotGetting = 1 - 27 / (100 * 11)
+    }
+    else if(activeBee.rarity == "Rare"){
+      chanceOfNotGetting = 1 - 7 / (10 * 9)
+    }
+    console.log("calculating")
+    let chanceOfGetting = (1 - Math.pow(chanceOfNotGetting, royalJellyCount)) * 100
+    setChance(chanceOfGetting)
+  }
+  let inputChange = (event) => {
+    var num = parseInt(event.target.value, 10)
+    setRoyalJellyCount(num)
+  }
   return (
     
     <div className="RoyalJelly">
       {(beeSelectIsOn == true) ? <BeeSelect beeSelectPosition={beeSelectPosition} callback={beeSelectCallBack}/> : <div></div>}
       <div className="Calculation">
         <h1>Odds Of Getting</h1>
-        <button onClick={beeSelectOn}><img src={"http://localhost:8000/static/beeimages/" + activeBee}></img></button>
+        <button onClick={beeSelectOn}><img src={"/static/beeimages/" + activeBee.name + ".png"}></img></button>
         
         <h1>from</h1>
-        <form><input value="1000"></input></form>
-        <img src="http://localhost:8000/static/miscellaneous/RoyalJelly.png"></img>
+        <input placeholder="1000" onChange={inputChange}></input>
+        <img src="/static/miscellaneous/RoyalJelly.png"></img>
+        <button onClick={calculate}>Calculate</button>
+        <p>{Math.round(chance * 1000) / 1000 + "%"}</p>
+        
       </div>
     </div>
   );
--- csvgeneratefromfilename.py
+++ csvgeneratefromfilename.py
@@ -1,4 +1,6 @@
 import os
 #os.mkdir("beenames.csv")
-for filename in os.listdir("static/beeimages"):
+for filename in os.listdir("bssoddsapp/public/static/beeimages"):
     open("beenames.txt", "a").write('<button onClick={() => props.callback("' + filename + '")}><img src="http://localhost:8000/static/beeimages/' + filename + '"></img></button>\n')
+    #open("csv.txt", "a").write(filename.split(".", 1)[0] + ', ')
+"Basicbee.png"

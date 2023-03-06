eed2950b3fb977c7d113278dca36b5ed8cb12313
Tezzy <Tezzy>
2023-03-06 17:49:08 +1100

    new

--- bssoddsapp/src/App.js
+++ bssoddsapp/src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       
 
-      < RoyalJelly />
+      <RoyalJelly isGifted={false}/>
+      <RoyalJelly isGifted={true}/>
     </div>
   );
 }
@@ -44,7 +45,7 @@ function BeeSelect(props){
     </div>
   );
 }
-function RoyalJelly() {
+function RoyalJelly(props) {
   let [src, setSrc] = useState("/static/beeimages/BasicBee.png");
   let [beeSelectIsOn, setbeeSelectIsOn] = useState(false);
   let [beeSelectPosition, setBeeSelectPosition] = useState({x: 600, y: 100});
@@ -69,16 +70,16 @@ function RoyalJelly() {
   let calculate = () => {
     let chanceOfNotGetting = 1
     if(activeBee.rarity == "Mythic"){
-      chanceOfNotGetting = 1 - 1 / (25000 * 6)
+      chanceOfNotGetting = 1 - 1 / (25000 * 6 * (props.isGifted ? 250 : 1))
     }
     else if(activeBee.rarity == "Legendary"){
-      chanceOfNotGetting = 1 - 3 / (100 * 8)
+      chanceOfNotGetting = 1 - 3 / (100 * 8 * (props.isGifted ? 250 : 1))
     }
     else if(activeBee.rarity == "Epic"){
-      chanceOfNotGetting = 1 - 27 / (100 * 11)
+      chanceOfNotGetting = 1 - 27 / (100 * 11 * (props.isGifted ? 250 : 1))
     }
     else if(activeBee.rarity == "Rare"){
-      chanceOfNotGetting = 1 - 7 / (10 * 9)
+      chanceOfNotGetting = 1 - 7 / (10 * 9 * (props.isGifted ? 250 : 1))
     }
     console.log("calculating")
     let chanceOfGetting = (1 - Math.pow(chanceOfNotGetting, royalJellyCount)) * 100
@@ -94,13 +95,15 @@ function RoyalJelly() {
       {(beeSelectIsOn == true) ? <BeeSelect beeSelectPosition={beeSelectPosition} callback={beeSelectCallBack}/> : <div></div>}
       <div className="Calculation">
         <h1>Odds Of Getting</h1>
+        {props.isGifted ? <h1 className='bold'>Gifted</h1> : null}
+        
         <button onClick={beeSelectOn}><img src={"/static/beeimages/" + activeBee.name + ".png"}></img></button>
         
-        <h1>from</h1>
-        <input placeholder="1000" onChange={inputChange}></input>
-        <img src="/static/miscellaneous/RoyalJelly.png"></img>
+        <h1>From</h1>
+        <input placeholder="0" onChange={inputChange}></input>
+        <img className='RoyalJellyImg' src="/static/miscellaneous/RoyalJelly.png"></img>
         <button onClick={calculate}>Calculate</button>
-        <p>{Math.round(chance * 1000) / 1000 + "%"}</p>
+        <h1>{Math.floor(chance * 1000) / 1000 + "%"}</h1>
         
       </div>
     </div>

822dff7e67a09a288d4e675a0952cb55d0d0def2
Tezzy <Tezzy>
2023-03-11 13:55:51 +1100

    fixed images on github pages

--- bssoddsapp/src/App.js
+++ bssoddsapp/src/App.js
@@ -19,7 +19,7 @@ function BeeSelect(props){
       top: `${props.beeSelectPosition.y}px`
   };
   useEffect(() => {
-    fetch("/static/miscellaneous/bees.json").then(res => res.json()).then(data => {
+    fetch("/bssodds/static/miscellaneous/bees.json").then(res => res.json()).then(data => {
       console.log(data.bees[0])
       setbees([])
       for (let i = 0; i < data.bees.length; i++) {
@@ -37,14 +37,14 @@ function BeeSelect(props){
       <h1>Choose a Bee</h1>
       <div className="BeeSelect">
         {bees.map(bee => (
-          <button onClick={() => props.callback(bee)}><img src={"/static/beeimages/" + bee.name + ".png"}></img></button>
+          <button onClick={() => props.callback(bee)}><img src={"/bssodds/static/beeimages/" + bee.name + ".png"}></img></button>
         ))}
       </div>
     </div>
   );
 }
 function RoyalJelly(props) {
-  let [src, setSrc] = useState("/static/beeimages/BasicBee.png");
+  let [src, setSrc] = useState("/bssodds/static/beeimages/BasicBee.png");
   let [beeSelectIsOn, setbeeSelectIsOn] = useState(false);
   let [beeSelectPosition, setBeeSelectPosition] = useState({x: 600, y: 100});
   let [activeBee, setActiveBees] = useState({name: "VectorBee", rarity: "Mythic"});
@@ -95,11 +95,11 @@ function RoyalJelly(props) {
         <h1>Odds Of Getting</h1>
         {props.isGifted ? <h1 className='bold'>Gifted</h1> : null}
         
-        <button onClick={beeSelectOn}><img src={"/static/beeimages/" + activeBee.name + ".png"}></img></button>
+        <button onClick={beeSelectOn}><img src={"/bssodds/static/beeimages/" + activeBee.name + ".png"}></img></button>
         
         <h1>From</h1>
         <input placeholder="0" onChange={inputChange}></input>
-        <img className='RoyalJellyImg' src="/static/miscellaneous/RoyalJelly.png"></img>
+        <img className='RoyalJellyImg' src="/bssodds/static/miscellaneous/RoyalJelly.png"></img>
         <button onClick={calculate}>Calculate</button>
         <h1>{Math.floor(chance * 1000) / 1000 + "%"}</h1>
         
